7d021afc1386b095c239ec4ed0faa14d6889bbad
Augustus <jr.jardimmatos@gmail.com>
2024-11-06 13:32:09 -0300

    refactor: unificando rota de websocket e consumer para channel global

--- notifications/ws_routing.py
+++
@@ -1,6 +0,0 @@
-# from django.conf.urls import url
-# from .consumers import NotificationsWebsocket
-
-# websocket_urlpatterns = [
-#     # url(r'^ws/notifications/(?P<channel_code>\w+)/$', NotificationsWebsocket.as_asgi()),
-# ]
